85ffa3326ee1d7dc61da0cedada5276eed9f5ad3
Mark Katerberg <katerberg@fastmail.fm>
2024-07-12 16:12:32 -0500

    Moves map generation to constructor

--- src/MapLevel.ts
+++ src/MapLevel.ts
@@ -1,15 +1,62 @@
-import {Cell, Coordinate} from '../types/sharedTypes';
+import {Map} from 'rot-js';
+import {dimensions} from '../types/constants';
+import {Cell, CellType, Coordinate, VisibilityStatus} from '../types/sharedTypes';
 import {Ladder} from './Ladder';
 
 export class MapLevel {
+  levelNumber: number;
+
   exits: Ladder[];
 
   cells: {
     [key: Coordinate]: Cell;
   };
 
-  constructor() {
+  constructor({levelNumber}: {levelNumber: number}) {
+    this.levelNumber = levelNumber;
     this.exits = [];
     this.cells = {};
+
+    for (let y = 0; y < dimensions.HEIGHT; y++) {
+      for (let x = 0; x < dimensions.WIDTH; x++) {
+        this.cells[`${x},${y}`] = {
+          x,
+          y,
+          isEntrance: false,
+          isExit: false,
+          isPassable: false,
+          isWalkable: false,
+          type: CellType.Wall,
+          visibilityStatus: VisibilityStatus.Unseen,
+          items: [],
+        };
+      }
+    }
+
+    const digger = new Map.Digger(Math.ceil(dimensions.WIDTH - 50 + Math.pow(levelNumber, 2) / 2), dimensions.HEIGHT, {
+      // dugPercentage: levelNumber * 0.1,
+      dugPercentage: 0.9,
+      corridorLength: [0, 5],
+    });
+
+    const digCallback = (x: number, y: number, value: number): void => {
+      if (value) {
+        return;
+      }
+
+      const key: Coordinate = `${x},${y}`;
+      this.cells[key] = {
+        x,
+        y,
+        isEntrance: false,
+        isExit: false,
+        isPassable: true,
+        isWalkable: true,
+        type: CellType.Earth,
+        visibilityStatus: VisibilityStatus.Unseen,
+        items: [],
+      };
+    };
+    digger.create(digCallback);
   }
 }
--- src/mapHelper.ts
+++ src/mapHelper.ts
@@ -1,6 +1,4 @@
-import {Map} from 'rot-js';
-import {dimensions} from '../types/constants';
-import {CellType, Coordinate, DungeonMap, VisibilityStatus} from '../types/sharedTypes';
+import {DungeonMap} from '../types/sharedTypes';
 import {MapLevel} from './MapLevel';
 
 export function generateMap(): DungeonMap {
@@ -8,48 +6,7 @@ export function generateMap(): DungeonMap {
     levels: [],
   };
   for (let levelNumber = 0; levelNumber < 10; levelNumber++) {
-    dungeonMap.levels[levelNumber] = new MapLevel();
-    for (let y = 0; y < dimensions.HEIGHT; y++) {
-      for (let x = 0; x < dimensions.WIDTH; x++) {
-        dungeonMap.levels[levelNumber].cells[`${x},${y}`] = {
-          x,
-          y,
-          isEntrance: false,
-          isExit: false,
-          isPassable: false,
-          isWalkable: false,
-          type: CellType.Wall,
-          visibilityStatus: VisibilityStatus.Unseen,
-          items: [],
-        };
-      }
-    }
-
-    const digger = new Map.Digger(Math.ceil(dimensions.WIDTH - 50 + Math.pow(levelNumber, 2) / 2), dimensions.HEIGHT, {
-      // dugPercentage: levelNumber * 0.1,
-      dugPercentage: 0.9,
-      corridorLength: [0, 5],
-    });
-
-    const digCallback = (x: number, y: number, value: number): void => {
-      if (value) {
-        return;
-      }
-
-      const key: Coordinate = `${x},${y}`;
-      dungeonMap.levels[levelNumber].cells[key] = {
-        x,
-        y,
-        isEntrance: false,
-        isExit: false,
-        isPassable: true,
-        isWalkable: true,
-        type: CellType.Earth,
-        visibilityStatus: VisibilityStatus.Unseen,
-        items: [],
-      };
-    };
-    digger.create(digCallback);
+    dungeonMap.levels[levelNumber] = new MapLevel({levelNumber});
   }
   return dungeonMap;
 }
